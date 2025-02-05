6c8d84b6b93a5d49ad7eddf92a3475e347f77326
mike <mike.hsn.work@gmail.com>
2025-02-05 17:50:12 +0200

    fu

--- src/stores/user.store.ts
+++ src/stores/user.store.ts
@@ -2,14 +2,30 @@ import { makeAutoObservable } from 'mobx'
 import { RootStore } from './RootStore'
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { invokeApiAction, loadable } from '../utils/store.utils'
+import { getAuth, onAuthStateChanged, User } from 'firebase/auth'
 // import { getOpenAiKey } from '../services/firebase/realtime.service'
 
 class UserStore {
   // apiKey = loadable('')
   test = loadable('initial value')
+  user: User | null = null
+  uid: string | null = null
 
   constructor(public root: RootStore) {
     makeAutoObservable(this)
+  }
+
+  getCurrentUUID() {
+    return
+  }
+
+  private initAuthListener() {
+    const auth = getAuth()
+
+    onAuthStateChanged(auth, (user) => {
+      this.user = user
+      this.uid = user ? user.uid : null
+    })
   }
 }
 
